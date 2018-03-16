904d114994158d825ee80ec0118b846af2e48a07
François Martineau <francoismartineau@github.com>
2018-03-16 02:30:52 -0400

    AutoCommit2018/03/16

--- auto_git.py
+++ auto_git.py
@@ -1,15 +1,18 @@
 import sys, os, subprocess, datetime
 
 """
-    Automatically push origin master a given repository.
-"""
-repository = sys.argv[1]
-now = datetime.datetime.now()
-date = "{0}/{1:02d}/{2:02d}".format(now.year,
-                                    now.month,
-                                    now.day)
+    \Brief: Automatically push origin master a given repository. The
+    \Brief: origin remote will be used by default.
+    \Argument [repository]: string that represents the path to the repository
 
-os.chdir(repository)
-subprocess.call(["git", "add", "."])
-subprocess.call(["git", "commit", "-m", "AutoCommit{0}".format(date)])
-subprocess.call(["git", "push", "origin", "master"])
+"""
+def auto_git(repository):
+    now = datetime.datetime.now()
+    date = "{0}/{1:02d}/{2:02d}".format(now.year,
+                                        now.month,
+                                        now.day)
+    
+    os.chdir(repository)
+    subprocess.call(["git", "add", "."])
+    subprocess.call(["git", "commit", "-m", "AutoCommit{0}".format(date)])
+    subprocess.call(["git", "push", "origin", "master"])
